1768b3e4ce38ac8c5206c1df8eeb569eba9108e0
TomixChiu <qhr2001@yahoo.com>
2018-09-18 12:25:10 +0800

    upload src

---
+++ src/ConfigurationFactory.php
@@ -0,0 +1,142 @@
+<?php
+namespace swiftphp\config;
+
+use swiftphp\config\internal\PhpConfiguration;
+use swiftphp\config\internal\XmlConfiguration;
+
+/**
+ * 配置工厂
+ * @author Tomix
+ *
+ */
+class ConfigurationFactory
+{
+    /**
+     * 配置类静态实例数组
+     * 键为配置文件，值为配置对象
+     */
+    private static $m_configs=[];
+
+    /**
+     * 持久化配置文件时的键
+     * @var string
+     */
+    private static $m_configFilesKey="_xml-source-config-files";
+
+    /**
+     * 调试模式开关
+     * @var string
+     */
+    private static $m_debug=false;
+
+    /**
+     * 调试模式开关
+     * @param bool $value
+     */
+    public static function setDebug($value)
+    {
+        self::$m_debug=$value;
+    }
+
+    /**
+     * 创建配置实例
+     * @param string $configFile 入口配置文件名
+     * @param string $baseDir    应用根目录
+     * @param string $userDir    用户根目录
+     * @param array  $extConfigs 附加扩展的配置(section,name,value形式的数组)
+     * @return IConfiguration
+     */
+    public static function create($configFile,$baseDir="",$userDir="",$extConfigs=[])
+    {
+        //配置文件
+        $pathInfo=pathinfo($configFile);
+        $_configFile=$pathInfo["dirname"]."/".$pathInfo["filename"];
+        $phpConfigFile=$_configFile.".php";
+        $xmlConfigFile=$_configFile.".xml";
+        $_configFile=file_exists($phpConfigFile)?$phpConfigFile:$xmlConfigFile;
+        if(!file_exists($_configFile)){
+            throw new \Exception("Fail to load configuration file '".$configFile."'!");
+        }
+
+        //如果缓存存在,则直接返回实例
+        $configKey=$_configFile;
+        if(array_key_exists($configKey, self::$m_configs)){
+            return self::returnConfig(self::$m_configs[$configKey],$baseDir,$userDir,$extConfigs);
+        }
+
+        //从php配置文件读取配置:如果php配置文件存在且修改时间大于所有的xml配置文件
+        if(file_exists($phpConfigFile)){
+            //从php读取配置
+            $config=new PhpConfiguration($phpConfigFile);
+
+            //如果xml配置文件不存在,则直接从php配置直接返回
+            if(!file_exists($xmlConfigFile)){
+                self::$m_configs[$configKey]=$config;
+                return self::returnConfig($config,$baseDir,$userDir,$extConfigs);
+            }
+
+            //如果xml配置文件存在,则要检查文件最后修改时间
+            $phpTime=filemtime($phpConfigFile);
+            $overrided=false;
+            foreach ($config->getConfigValues(self::$m_configFilesKey) as $f){
+                if(file_exists($f) && filemtime($f) > $phpTime){
+                    $overrided=true;
+                }
+            }
+
+            //php文件较新且非调试模式下
+            if(!$overrided && !self::$m_debug){
+                self::$m_configs[$configKey]=$config;
+                return self::returnConfig($config,$baseDir,$userDir,$extConfigs);
+            }
+        }
+
+        //从xml配置读取配置,并持久化到php
+        if(file_exists($xmlConfigFile)){
+            $config=new XmlConfiguration($xmlConfigFile);
+            self::$m_configs[$configKey]=$config;
+            self::dump2PhpFile($config, $phpConfigFile);
+        }
+
+        //返回实例
+        if(array_key_exists($configKey, self::$m_configs)){
+            return self::returnConfig(self::$m_configs[$configKey],$baseDir,$userDir,$extConfigs);
+        }
+        return null;
+    }
+
+    /**
+     * 返回的配置实例
+     * @param IConfiguration $config
+     * @param string $baseDir
+     * @param string $userDir
+     * @param array $extConfigs
+     * @return \swiftphp\core\config\IConfiguration
+     */
+    private static function returnConfig(IConfiguration $config,$baseDir="",$userDir="",$extConfigs=[])
+    {
+        $config->setBaseDir($baseDir);
+        $config->setUserDir($userDir);
+        if(!empty($extConfigs)){
+            foreach ($extConfigs as $ext){
+                $config->addConfigValue($ext["section"], $ext["name"], $ext["value"]);
+            }
+        }
+        return $config;
+    }
+
+    /**
+     * 把配置内容持久化成php文件
+     * @param IConfiguration $config
+     * @param string $file
+     */
+    private static function dump2PhpFile(IConfiguration $config,$file)
+    {
+        $values=$config->getAllValues();
+        $values[self::$m_configFilesKey]=$config->getConfigFiles();
+        $content=var_export($values, TRUE);
+        $content="<?php\r\nreturn\r\n".$content.";";
+        file_put_contents($file, $content);
+    }
+
+}
---
+++ src/IObjectFactory.php
@@ -0,0 +1,25 @@
+<?php
+namespace swiftphp\config;
+
+/**
+ * 对象工厂接口
+ * @author Tomix
+ *
+ */
+interface IObjectFactory
+{
+    /**
+     * 根据对象ID创建实例
+     * @param string $objectId  对象ID
+     */
+    function create($objectId);
+
+    /**
+     * 根据类型名创建实例
+     * @param string $class             类型名称
+     * @param array $constructorArgs    构造参数(对于已配置的类型,该参数忽略)
+     * @param bool $singleton           是否单例模式,默认为单例模式(对于已配置的类型,该参数忽略)
+     */
+    function createByClass($class,$constructorArgs=[],$singleton=true);
+}
+
